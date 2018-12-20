437f0cbd377c2f3ca9cb9e250ae470d18d0eaf6c
D4rthB4n3 <dessel.bane@gmx.de>
2018-12-20 13:48:22 +0100

    fixed animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {StorageModule} from "./storage/storage.module";
 import {MaterialMetaModule} from "./material-meta/material-meta.module";
 import {RecipesModule} from "./recepies/recipes.module";
@@ -22,8 +21,7 @@ import {CalculatorModule} from "./calculator/calculator.module";
     RecipesModule,
     CalculatorModule,
 
-    AppRoutingModule,
-    NoopAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
